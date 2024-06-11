86c88d74f2b74961284b494fb6aeaae3a1769cd3
Bluemangoo <chenfy2006@qq.com>
2024-06-12 00:18:03 +0800

    add vercel cache

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,15 @@
-import VCLight from "vclight";
+import VCLight, { RawVercelRequest, VCLightRequest, VCLightResponse } from "vclight";
 import router from "./router";
 import "./initRouter";
 
 const app = new VCLight();
+app.use({
+    async post(request: VCLightRequest, response: VCLightResponse, app: VCLight) {
+        if (request.rawRequest instanceof RawVercelRequest) {
+            request.rawRequest.response.setHeader("Vercel-CDN-Cache-Control", "max-age=600");
+        }
+    },
+    async process(request: VCLightRequest, response: VCLightResponse, app: VCLight) {}
+});
 app.use(router);
 export default app;
